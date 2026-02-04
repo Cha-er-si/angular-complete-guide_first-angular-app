39445f4a11a1b381cae7afc93ce617421ba6bf3c
Cha-er-si <princecharles.velarde@gmail.com>
2026-02-04 21:43:23 +0800

    [exercise] configurable component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,18 +3,21 @@ import { HeaderComponent } from './components/header/header';
 import { UserComponent } from './components/user/user';
 import { UserInterface } from './app.interface';
 import { DUMMY_USERS } from './app.const';
+import { TasksComponent } from './components/tasks/tasks';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   users: UserInterface[] = DUMMY_USERS;
+  selectedTask: string = '';
 
   onSelectUser(id: string) {
     console.log('User Selected: ' + id);
+    this.selectedTask = this.users.find((user) => user.id === id)?.name ?? '';
   }
 }
